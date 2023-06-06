f5db1d9fbeda2d8f50125086340e07f6c1e2accf
Poojan <58319619+kaneriyapoojan@users.noreply.github.com>
2023-06-06 15:27:28 +0530

    Delete js directory

--- js/main.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.querySelector('.hamburger');
-const menu = document.getElementById('menu');
-
-hamburger.addEventListener('click', function(){
-    this.classList.toggle('is-active')
-    // menu.style.display = "flex";
-});
